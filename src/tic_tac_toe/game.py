58e6ae109536738774c570cfc8109f87b0a215c7
Erin Akinjide <erinakinjide16@gmail.com>
2023-10-19 09:53:51 -0400

    Major overhaul of core game logic structure and methods. Condensed command within each method.

--- src/tic_tac_toe/game.py
+++ src/tic_tac_toe/game.py
@@ -2,99 +2,41 @@ import pyfiglet
 
 class TicTacToe:
     def __init__(self):
-        """
-        Initializes a new instance of the Tic Tac Toe game.
+        self.board = [' ' for _ in range(9)]
+        self.winner = None
+        self.current_player = 'X'
+        self.moves_count = 0
 
-        The game board is represented as a list of 9 empty spaces.
-        """
-        self.board = [' ' for _ in range(9)]  
+        # Display welcome message
+        print(pyfiglet.figlet_format("Welcome to Tic Tac Toe"))
 
     def print_board(self):
-        """
-        Prints the current state of the board.
-        """
-        
-        # Start a new line for every 3 spaces in the board.
-        for i in range(0, 9, 3):
-
-            # Print the current row of the board.
-            print(f'{self.board[i]} | {self.board[i + 1]} | {self.board[i + 2]}')
-            # Print a line of dashes after every row except the last
-            if i < 6:
-                print('-' * 9)
+        print(f'{self.board[0]} | {self.board[1]} | {self.board[2]}')
+        print('--|---|--')
+        print(f'{self.board[3]} | {self.board[4]} | {self.board[5]}')
+        print('--|---|--')
+        print(f'{self.board[6]} | {self.board[7]} | {self.board[8]}')
 
     def make_move(self, position, player):
-        """
-        Makes a move on the board.
-
-        Args:
-            position (int): The position on the board where the move is made (0-8).
-            player (str): The player making the move ('X' or 'O').
-
-        Returns:
-            bool: True if the move was successful, False if the position is already taken.
-        """
-
-        # Check if the position is empty.
         if self.board[position] == ' ':
-            
-            # If the position is empty, set the position to the player.
             self.board[position] = player
+            self.moves_count += 1
             return True
         else:
+            print(f"Position {position} is already taken. Please choose another position.")
             return False
-    
-    def check_winner(self, position):
-        """
-        Determines the winner by checking the rows, columns, and diagonals that contain the last move made on the board.
 
-        Args:
-            position (int): The position on the board where the last move was made (0-8).
+    def check_winner(self):
+        win_conditions = [(0, 1, 2), (3, 4, 5), (6, 7, 8), (0, 3, 6), (1, 4, 7), (2, 5, 8), (0, 4, 8), (2, 4, 6)]
 
-        Returns:
-            str or None: The winning player ('X' or 'O'), 'Draw' for a draw, or None if no winner yet.
-        """
-          # Extract the row and column from the position on the board.
-        row = position // 3
-        col = position % 3
-        # Use the position to extract the player from the board.
-        player = self.board[position]
+        for condition in win_conditions:
+            if self.board[condition[0]] == self.board[condition[1]] == self.board[condition[2]] != ' ':
+                self.winner = self.board[condition[0]]
+                return True
 
-        # Check row to see if it matches the player from the position.
-        if self.board[row * 3] == self.board[row * 3 + 1] == self.board[row * 3 + 2] == player:
-            return player
+        if self.moves_count == 9 and self.winner is None:
+            self.winner = 'Draw'
+            return True
 
-        # Check column to see if it matches the player from the position.
-        if self.board[col] == self.board[col + 3] == self.board[col + 6] == player:
-            return player
+        return False
 
-        # Check diagonal to see if it matches the player from the position.
-        if position % 2 == 0:
-            if self.board[0] == self.board[4] == self.board[8] == player or self.board[2] == self.board[4] == self.board[6] == player:
-                return player
-
-        # Show that there is a draw if there are no empty spaces left on the board.
-        if ' ' not in self.board:
-            return 'Draw'
-
-        return None
-
-    def print_welcome_message(self):
-        """
-        Prints a welcome message using pyfiglet.
-        """
-        welcome_message = pyfiglet.figlet_format('Welcome to Tic Tac Toe!', font='standard')
-        print(welcome_message)
-
-    def display_winner_message(self, winner):
-        """
-        Display a stylized message announcing the winner.
-
-        Args:
-            winner (str): The winning player ('X' or 'O').
-        """
-        if winner == 'Draw':
-            message = pyfiglet.figlet_format('It\'s a Draw!', font='standard')
-        else:
-            message = pyfiglet.figlet_format(f'Player {winner} Wins!', font='standard')
-        print(message)
